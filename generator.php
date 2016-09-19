4b07b376de877b91f69c69c89a0dd40d39ac5578
Alec O'Connor <alec@alecoconnor.com>
2016-09-18 23:36:39 -0400

    Style down, input to come
    Style of table is ready and the ability to make changes to the puzzle
    are to come!

--- generator.php
+++ generator.php
@@ -8,18 +8,18 @@ $puzzle = array();
 
 //add columns individually to array
 array_push($puzzle,
-array(0,2,0,1,7,8,0,3,0),
-array(0,4,0,3,0,2,0,9,0),
-array(1,0,0,0,0,0,0,0,6),
-array(0,0,8,6,0,3,5,0,0),
-array(3,0,0,0,0,0,0,0,4),
-array(0,0,6,7,0,9,2,0,0),
-array(9,0,0,0,0,0,0,0,2),
-array(0,8,0,9,0,1,0,6,0),
-array(0,1,0,4,3,6,0,5,0)
+array('',2,'',1,7,8,'',3,''),
+array('',4,'',3,'',2,'',9,''),
+array(1,'','','','','','','',6),
+array('','',8,6,'',3,5,'',''),
+array(3,'','','','','','','',4),
+array('','',6,7,'',9,2,'',''),
+array(9,'','','','','','','',2),
+array('',8,'',9,'',1,'',6,''),
+array('',1,'',4,3,6,'',5,'')
 );
 
 //output JSON
-echo json_encode($puzzle);
+echo json_encode($puzzle, JSON_FORCE_OBJECT);
 
 ?>
--- index.php
+++ index.php
@@ -2,30 +2,40 @@
 <head>
 <title>Sudoku Challenge</title>
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
-
+<link rel="stylesheet" type="text/css" href="style/style.css">
 </head>
 <body>
-<div ng-app="sudoku" ng-controller="sudokuController">
-  <div ng-repeat="y in puzzle">
-    <div ng-repeat="x in y">
-    
+
+<div id="intro">
+<h1>The Great Sudoku Challenge</h1>
+<h3>Be the first to beat our classic Sudoku challenge for an all-expenses-paid week-long vacation!</h3>
+</div>
+
+<div id="puzzle" ng-app="sudoku" ng-controller="sudokuController">
+  <div class="row" ng-repeat="y in puzzle">
+    <div class="column" ng-repeat="x in y" ng-init="column=$index+1,row=y.">
+      {{x}}
     </div>
+    <div class="clear"></div>
   </div>
 </div>
 
-<table>
-  <tr ng-repeat="x in puzzle">
-    <td>{{ x.Name }}</td>
-  </tr>
-</table>
-
+<div id="rules">
+<p>The classic sudoku game involves a grid of 81 squares. The grid is divided into nine blocks, each containing nine squares.</p>
+<p>The rules of the game are simple: each of the nine blocks must contain all the numbers 1-9 within its squares. Each number can only appear once in a row, column, or box.</p>
+<p>The difficulty lies in that each vertical nine-square column, or horizontal nine-square line across, within the larger square, must also contain the numbers 1-9, without repetition or omission.</p>
+<p>Every puzzle has just one correct solution.</p>
+</div>
 
 <script>
-var app = angular.module('myApp', []);
+var app = angular.module('sudoku', []);
 app.controller('sudokuController', function($scope, $http) {
     $http.get("generator.php").then(
     function (response) {
       $scope.puzzle = response.data;
+      $scope.checkLegalEntries = function() {
+
+      };
     });
 });
 </script>
